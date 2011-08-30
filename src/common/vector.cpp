b082c7f3d5982d8c41eca7b6c4a9d971b75bde8d
Alex Pivovarov <aleksandr.pivovarov.84@gmail.com>
2011-08-30 23:58:00 +0300

    reserv

---
+++ src/common/vector.cpp
@@ -0,0 +1,14 @@
+#include "vector.h"
+                
+vec2f :: vec2f()
+{
+        x = 0;
+        y = 0;
+}
+                
+void vec2f :: set(float _x, float _y) 
+{ 
+        x = _x; 
+        y = _y; 
+}
+
---
+++ src/common/vector.h
@@ -0,0 +1,24 @@
+#ifndef VECTOR_H
+#define VECTOR_H
+
+class vec2f
+{
+        public:
+                float x;
+                float y;
+                
+                vec2f();
+                
+                void set(float _x, float _y);
+};
+
+class vec3f
+{
+        public:
+                float x;
+                float y;
+                float z;
+};
+
+
+#endif
