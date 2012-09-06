5e5f9084bfa9af5c96302c55c2090668f00458b8
Alex Pivovarov <amdlintuxos@gmail.com>
2012-09-06 10:27:23 +0300

    gui vehicle, gui skill, offset improvement

--- src/common/myVector.hpp
+++ src/common/myVector.hpp
@@ -1,3 +1,21 @@
+/*
+        Copyright (C) ColdStars, Aleksandr Pivovarov <<coldstars8@gmail.com>>
+        
+        This program is free software; you can redistribute it and/or
+        modify it under the terms of the GNU General Public License
+        as published by the Free Software Foundation; either version 2
+        of the License, or (at your option) any later version.
+        
+        This program is distributed in the hope that it will be useful,
+        but WITHOUT ANY WARRANTY; without even the implied warranty of
+        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+        GNU General Public License for more details.
+        
+        You should have received a copy of the GNU General Public License
+        along with this program; if not, write to the Free Software
+        Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
 #ifndef MYVECTOR_H
 #define MYVECTOR_H
 
@@ -61,6 +79,8 @@ class vec2i
                 vec2i(int x, int y) { this->x = x; this->y = y; };
                 vec2i(const vec2i& v) { *this = v; } ;
                 ~vec2i() {};
+                
+                vec2i operator+(const vec2i&) const;
                 		
 };
 
