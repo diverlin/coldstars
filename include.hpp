45958418f35a44ed9d4389aefce7dad4a12677b7
Alex Pivovarov <amdlintuxos@gmail.com>
2012-09-18 23:04:59 +0300

    code refactoring

--- include.hpp
+++ include.hpp
@@ -117,7 +117,6 @@ class Kosmoport;
 #include "src/gui/GuiManager.cpp"
 #include "src/gui/GuiVehicle.cpp"
 #include "src/gui/GuiVehicle2.cpp"
-#include "src/gui/GuiSkill.cpp"
 #include "src/gui/GuiMap.cpp"
 #include "src/gui/Cursor.cpp"   
 #include "src/gui/UserInput.cpp"
