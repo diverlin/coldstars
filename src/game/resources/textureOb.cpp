08d866a62be83ffa5b93c8f8506b5f9edb0876a0
Alex Pivovarov <amdlintuxos@gmail.com>
2013-06-17 23:11:56 +0300

    mipmap on, bug fix in not defined button color(cause artefact in gui)

--- src/game/resources/textureOb.cpp
+++ src/game/resources/textureOb.cpp
@@ -434,9 +434,9 @@ void loadToVRAM(const std::string& path, GLuint& texture, int& w, int& h)
      	glGenTextures(1, &texture);
      	glBindTexture(GL_TEXTURE_2D, texture);
      	gluBuild2DMipmaps(GL_TEXTURE_2D, GL_RGBA, w, h, GL_RGBA, GL_UNSIGNED_BYTE, image.getPixelsPtr());
-        
-     	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NICEST);
-     	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NICEST);
+         
+     	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+     	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
 }         
 
 
