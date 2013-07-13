56d7b302ad21c65907e06981e0e2b7d18f0878b7
Alex Pivovarov <amdlintuxos@gmail.com>
2013-07-13 15:18:49 +0300

    code clean up, renames, encapsulation for Base::

--- src/game/common/Base.cpp
+++ src/game/common/Base.cpp
@@ -47,10 +47,10 @@ void Base::SaveDataUniqueBase(boost::property_tree::ptree& save_ptree, const std
 	Logger::Instance().Log(" Base("+int2str(GetId())+")::SaveDataUniqueBase", SAVELOAD_LOG_DIP);
 	#endif
 	
-	save_ptree.put(root+"data_id.id",            data_id.id);
-	save_ptree.put(root+"data_id.type_id",       (int)data_id.type_id);
-	save_ptree.put(root+"data_id.subtype_id",    (int)data_id.subtype_id);
-	save_ptree.put(root+"data_id.subsubtype_id", (int)data_id.subsubtype_id);
+	save_ptree.put(root+"data_id.id",            m_Data_id.id);
+	save_ptree.put(root+"data_id.type_id",       (int)m_Data_id.type_id);
+	save_ptree.put(root+"data_id.subtype_id",    (int)m_Data_id.subtype_id);
+	save_ptree.put(root+"data_id.subsubtype_id", (int)m_Data_id.subsubtype_id);
 }
 
 void Base::LoadDataUniqueBase(const boost::property_tree::ptree& load_ptree)
@@ -59,10 +59,10 @@ void Base::LoadDataUniqueBase(const boost::property_tree::ptree& load_ptree)
 	Logger::Instance().Log(" Base("+int2str(GetId())+")::LoadDataUniqueBase", SAVELOAD_LOG_DIP);
 	#endif
 	
-	data_id.id              = load_ptree.get<unsigned long int>("data_id.id");
-	data_id.type_id         = (ENTITY::eTYPE)load_ptree.get<int>("data_id.type_id");
-	data_id.subtype_id      = (ENTITY::eTYPE)load_ptree.get<int>("data_id.subtype_id");
-	data_id.subsubtype_id   = (ENTITY::eTYPE)load_ptree.get<int>("data_id.subsubtype_id");
+	m_Data_id.id              = load_ptree.get<unsigned long int>("data_id.id");
+	m_Data_id.type_id         = (ENTITY::eTYPE)load_ptree.get<int>("data_id.type_id");
+	m_Data_id.subtype_id      = (ENTITY::eTYPE)load_ptree.get<int>("data_id.subtype_id");
+	m_Data_id.subsubtype_id   = (ENTITY::eTYPE)load_ptree.get<int>("data_id.subsubtype_id");
 }
 
 void Base::ResolveDataUniqueBase()
