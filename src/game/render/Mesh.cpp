37d310cce6241e51bf0f7655dd948b38174fdc81
Aleksandr Pivovarov <amdlintuxos@gmail.com>
2013-11-13 13:50:34 +0200

    Mesh class improovement, clean up

--- src/game/render/Mesh.cpp
+++ src/game/render/Mesh.cpp
@@ -15,19 +15,25 @@ m_TextureOb(textureOb),
 m_VertexCount(0),
 m_ListId(0),
 m_VaoId(0),        
-m_VboId(0)
-{        
+m_VboId(0),
+m_HasNormals(true)
+{     
+    m_ListId = glGenLists(1);   
+ 	glGenVertexArrays(1, &m_VaoId);
+    glGenBuffers(1, &m_VboId);
+
     ObjLoader objLoader(path);
     
-    BuildFaces(objLoader);
+    FillVertices(objLoader);
     m_BoundaryBox = objLoader.GetBoundaryBox();
-     
-    //CreateList();   
-    CreateVbo();
 }
 
-void Mesh::BuildFaces(const ObjLoader& objLoader)
+void Mesh::FillVertices(const ObjLoader& objLoader)
 {
+    m_PrimitiveType = GL_TRIANGLES;
+
+    m_HasNormals = true;
+
     m_Vertices.clear();
 
     for(unsigned int i=0; i<objLoader.faces_indexes.size(); ++i)
@@ -41,12 +47,70 @@ void Mesh::BuildFaces(const ObjLoader& objLoader)
 
             m_Vertices.push_back(vertex);
         }
-    }
+    } 
+
+    m_VertexCount = m_Vertices.size();
+
+    UpdateVbo();
 }
 
-void Mesh::CreateList()
+void Mesh::FillVertices(const std::vector<glm::vec3>& positions, const std::vector<glm::vec2>& texcoords)
 {
-    m_ListId = glGenLists(1);
+    m_PrimitiveType = GL_POINTS;
+
+    m_HasNormals = false;
+
+    m_Vertices.clear();
+
+    for(unsigned int i=0; i<positions.size(); ++i)
+    {
+        Vertex vertex;
+        vertex.position = positions[i];
+        vertex.texcoord = texcoords[i];
+        vertex.normal   = glm::vec3(0.0f);
+        m_Vertices.push_back(vertex);
+    } 
+
+    m_VertexCount = m_Vertices.size();
+
+    UpdateVbo();
+}
+
+//void FillVerticesFast(const std::vector<glm::vec3>& positions, const std::vector<glm::vec2>& texcoords) const
+//{
+    //m_VertexCount = positions.size();
+
+    //int positions_buffer_length = m_VertexCount * 3;
+    //int texcoords_buffer_length = m_VertexCount * 2;
+    
+    //int data_buffer_lenght = positions_buffer_length + texcoords_buffer_length;  
+    //GLfloat data_array[data_buffer_lenght];        
+           
+    //uint32_t k=0;
+    //for (uint32_t i=0; i<m_VertexCount; i++)
+    //{
+        //data_array[k++] = positions[i].x; 
+        //data_array[k++] = positions[i].y;
+        //data_array[k++] = positions[i].z;
+        
+        //data_array[k++] = texcoords[i].x; 
+        //data_array[k++] = texcoords[i].y;
+    //}
+
+	//glBindVertexArray(m_VaoId);
+    //glBindBuffer(GL_ARRAY_BUFFER, m_VboId);
+    //glBufferData(GL_ARRAY_BUFFER, sizeof(data_array), data_array, GL_STATIC_DRAW);      
+
+    //// attribute pointers    
+    //glVertexAttribPointer(VERT_POSITION, 3, GL_FLOAT, GL_FALSE, (3+2+3)*sizeof(GLfloat), (const GLvoid*)0);
+	//glEnableVertexAttribArray(VERT_POSITION);
+
+	//glVertexAttribPointer(VERT_TEXCOORD, 2, GL_FLOAT, GL_FALSE, (3+2+3)*sizeof(GLfloat), (const GLvoid*)(3*sizeof(GLfloat)));
+	//glEnableVertexAttribArray(VERT_TEXCOORD);
+//}
+
+void Mesh::UpdateList()
+{
     glNewList(m_ListId, GL_COMPILE);
     for(unsigned int i=0; i<m_Vertices.size(); i++)
     { 
@@ -62,19 +126,22 @@ void Mesh::CreateList()
 }
 
   
-void Mesh::CreateVbo()
+void Mesh::UpdateVbo()
 {    
-    m_VertexCount = m_Vertices.size();
-
     int positions_buffer_length = m_VertexCount * 3;
     int texcoords_buffer_length = m_VertexCount * 2;
     int normals_buffer_length   = m_VertexCount * 3;
     
-    int data_buffer_lenght = positions_buffer_length + texcoords_buffer_length + normals_buffer_length;  
+    int data_buffer_lenght = positions_buffer_length + texcoords_buffer_length;
+    if (m_HasNormals)
+    { 
+        data_buffer_lenght += normals_buffer_length;  
+    } 
+
     GLfloat data_array[data_buffer_lenght];        
            
     uint32_t k=0;
-    for (uint32_t i=0; i<m_Vertices.size(); i++)
+    for (uint32_t i=0; i<m_VertexCount; i++)
     {
         data_array[k++] = m_Vertices[i].position.x; 
         data_array[k++] = m_Vertices[i].position.y;
@@ -82,17 +149,16 @@ void Mesh::CreateVbo()
         
         data_array[k++] = m_Vertices[i].texcoord.x; 
         data_array[k++] = m_Vertices[i].texcoord.y;
-        
-        data_array[k++] = m_Vertices[i].normal.x; 
-        data_array[k++] = m_Vertices[i].normal.y;
-        data_array[k++] = m_Vertices[i].normal.z;
+
+        if (m_HasNormals)
+        {         
+            data_array[k++] = m_Vertices[i].normal.x; 
+            data_array[k++] = m_Vertices[i].normal.y;
+            data_array[k++] = m_Vertices[i].normal.z;
+        }
     }
  
- 	glGenVertexArrays(1, &m_VaoId);
 	glBindVertexArray(m_VaoId);
-                   
-    glGenBuffers(1, &m_VboId);
-
     glBindBuffer(GL_ARRAY_BUFFER, m_VboId);
     glBufferData(GL_ARRAY_BUFFER, sizeof(data_array), data_array, GL_STATIC_DRAW);      
 
@@ -103,9 +169,12 @@ void Mesh::CreateVbo()
 	glVertexAttribPointer(VERT_TEXCOORD, 2, GL_FLOAT, GL_FALSE, (3+2+3)*sizeof(GLfloat), (const GLvoid*)(3*sizeof(GLfloat)));
 	glEnableVertexAttribArray(VERT_TEXCOORD);
 
-	glVertexAttribPointer(VERT_NORMAL, 3, GL_FLOAT, GL_FALSE,   (3+2+3)*sizeof(GLfloat), (const GLvoid*)(3*sizeof(GLfloat) + 2*sizeof(GLfloat)));
-	glEnableVertexAttribArray(VERT_NORMAL);
-} 
+    if (m_HasNormals)
+    { 
+	    glVertexAttribPointer(VERT_NORMAL, 3, GL_FLOAT, GL_FALSE,   (3+2+3)*sizeof(GLfloat), (const GLvoid*)(3*sizeof(GLfloat) + 2*sizeof(GLfloat)));
+	    glEnableVertexAttribArray(VERT_NORMAL);
+    }
+}     
   
 void Mesh::DrawVbo() const
 {
