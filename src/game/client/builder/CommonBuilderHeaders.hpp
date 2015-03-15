f9a2134f47d94f21dc1b437beb7cde707dd8cba3
Aleksandr Pivovarov <amdlintuxos@gmail.com>
2015-03-15 16:22:03 +0200

    addremove src

---
+++ src/game/client/builder/CommonBuilderHeaders.hpp
@@ -0,0 +1,37 @@
+/*
+    Copyright (C) ColdStars, Aleksandr Pivovarov <<coldstars8@gmail.com>>
+    
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License
+    as published by the Free Software Foundation; either version 2
+    of the License, or (at your option) any later version.
+    
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+#pragma once
+
+//#include <types/MeshTypes.hpp>
+
+#include <common/common.hpp>
+#include <math/rand.hpp>
+#include <common/IdGenerator.hpp>
+#include <common/Logger.hpp>
+
+//#include <resources/TextureCollector.hpp>
+//#include <jeti/TextureOb.hpp>
+//#include <resources/MeshCollector.hpp>
+
+#include <common/Global.hpp>
+#include <managers/EntitiesManager.hpp>
+
+//#include <jeti/animations/AnimationConstantRotation.hpp>
+//#include <jeti/animations/AnimationWiggle.hpp>
+
