c5a5c0e4a77328e334449674b71f3628030f1ba6
Aleksandr Pivovarov <amdlintuxos@gmail.com>
2015-11-13 17:02:35 +0200

    check point

--- src/game/core/communication/Message.hpp
+++ src/game/core/communication/Message.hpp
@@ -3,6 +3,8 @@
 #include <types/IdType.hpp>
 #include <types/TelegramTypes.hpp>
 
+#include <string>
+
 class Message
 {
     public:
@@ -17,30 +19,4 @@ class Message
         double delay = 0.0;
         double dispatch_time = 0.0;
 };
-
-
-//class CreateStarsystemEvent {
-//    public:
-//        CreateStarsystemEvent(const ID& agressor, const ID& victim, int damage);
-//        CreateStarsystemEvent(const std::string& data);
-
-//        ID starsystem = 0;
-
-//    std::string data() const;
-//};
-
-//class HitEvent {
-//    public:
-//        HitEvent(const ID& agressor, const ID& victim, int damage);
-//        HitEvent(const std::string& data);
-
-//        ID agressor = 0;
-//        ID victim = 0;
-//        int damage = 0;
-
-//    std::string data() const;
-//};
-
-
-//Message getMessage(const HitEvent& hitEvent, double delay = 0.0);
 
