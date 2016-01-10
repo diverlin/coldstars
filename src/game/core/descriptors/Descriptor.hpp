eca646c1f2697942e40397ac70d6a550945712be
Oleksandr Pyvovarov <aleksandr.pivovarov.84@gmail.com>
2016-01-10 22:27:54 +0200

    addremove files

---
+++ src/game/core/descriptors/Descriptor.hpp
@@ -0,0 +1,64 @@
+/*
+    Copyright (C) ColdStars, Aleksandr Pivovarov <<coldstars8@gmail.com>>
+    
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License
+    as published by the Free Software Foundation; either version 2
+    of the License, or (at your option) any later version.
+    
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+#pragma once
+
+#include <types/IdType.hpp>
+
+#include <map>
+#include <string>
+#include <boost/serialization/map.hpp>
+#include <boost/archive/text_oarchive.hpp>
+#include <boost/archive/text_iarchive.hpp>
+
+const std::string KEY_ID = "id";
+const std::string KEY_DAMAGE = "damage";
+const std::string KEY_RADIUS = "radius";
+
+class Descriptor
+{
+public:
+    std::map<std::string, std::string> map;
+
+    Descriptor(const std::map<std::string, std::string>&);
+    Descriptor(const std::string& data);
+    ~Descriptor() {}
+
+    std::string data() const;
+
+    bool operator==(const Descriptor& rhs) const;
+    const std::string& get_raw(const std::string& key) const;
+    template<class var_type>
+    var_type get(const std::string& key) const
+    {
+        std::istringstream ss(get_raw(key));
+        var_type value;
+        ss >> value;
+        std::cout << "get key=" << key <<"value=" << value;
+        return value;
+    }
+
+
+private:
+    friend class boost::serialization::access;
+    template<class Archive>
+    void serialize(Archive & ar, const unsigned int version)
+    {
+        ar & map;
+    }
+}; 
