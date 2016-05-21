48c06cbc5597b6685b2ed971ed8fd2d5532276c6
Oleksandr Pivovarov <Oleksandr Pivovarov@localhost>
2016-05-22 02:18:04 +0300

    checkpoint

--- src/game/core/descriptors/DescriptorGenerator.cpp
+++ src/game/core/descriptors/DescriptorGenerator.cpp
@@ -59,6 +59,29 @@ descriptor::Base generateStarSystemDescriptor(int race)
 }
 
 // items
+namespace {
+void addItemCommonFields(descriptor::Base& descriptor,
+                         id_type id,
+                         int type,
+                         int race,
+                         int tech_level,
+                         int modules_num,
+                         int mass,
+                         int condition,
+                         int deterioration,
+                         int price) {
+    descriptor.add(descriptor::Key::ID, global::get().idGenerator().nextId());
+    descriptor.add(descriptor::Key::TYPE, int(descriptor::Base::Type::RADAR));
+    descriptor.add(descriptor::Key::RACE, race);
+    descriptor.add(descriptor::Key::TECH, tech_level);
+    descriptor.add(descriptor::Key::MODULES_NUM, modules_num);
+    descriptor.add(descriptor::Key::MASS, mass);
+    descriptor.add(descriptor::Key::CONDITION, condition);
+    descriptor.add(descriptor::Key::DETERIORATION, deterioration);
+    descriptor.add(descriptor::Key::PRICE, price);
+}
+} // namespace
+
 descriptor::Base generateBakDescriptor(int race, int tech_level)
 {
     if (race == -1) {
@@ -71,8 +94,9 @@ descriptor::Base generateBakDescriptor(int race, int tech_level)
     int modules_num_max        = meti::getRandInt(EQUIPMENT::BAK::MODULES_NUM_MIN, EQUIPMENT::BAK::MODULES_NUM_MAX);
     int mass                   = meti::getRandInt(EQUIPMENT::BAK::MASS_MIN, EQUIPMENT::BAK::MASS_MAX);
     int condition_max          = meti::getRandInt(EQUIPMENT::BAK::CONDITION_MIN, EQUIPMENT::BAK::CONDITION_MAX);
-    int deterioration_normal = 1;
+    int deterioration = 1;
     int price = meti::getRandInt(100, 1000);
+
     int fuel_max_orig = meti::getRandInt(EQUIPMENT::BAK::FUEL_MIN, EQUIPMENT::BAK::FUEL_MAX) * (1 + EQUIPMENT::BAK::FUEL_TECH_RATE * (int)tech_level);
 
     //jeti::Mesh* mesh = MeshCollector::Instance().getMesh(TYPE::MESH::PLANE_ID);
@@ -80,15 +104,10 @@ descriptor::Base generateBakDescriptor(int race, int tech_level)
     //item_texOb = TEXTURE_MANAGER.returnItemTexOb(TYPE::TEXTURE::RADAR_EQUIPMENT_ID, revision_id)
 
     descriptor::Base descriptor;
-    descriptor.add(descriptor::Key::ID, global::get().idGenerator().nextId());
-    descriptor.add(descriptor::Key::TYPE, int(descriptor::Base::Type::BAK));
-    descriptor.add(descriptor::Key::RACE, race);
-    descriptor.add(descriptor::Key::TECH, tech_level);
-    descriptor.add(descriptor::Key::MODULES_NUM, modules_num_max);
-    descriptor.add(descriptor::Key::MASS, mass);
-    descriptor.add(descriptor::Key::CONDITION, condition_max);
-    descriptor.add(descriptor::Key::DETERIORATION, deterioration_normal);
-    descriptor.add(descriptor::Key::PRICE, price);
+    addItemCommonFields(descriptor,
+                        global::get().idGenerator().nextId(),
+                        int(descriptor::Base::Type::BAK),
+                        race, tech_level, modules_num_max, mass, condition_max, deterioration, price);
     descriptor.add(descriptor::Key::FUEL, fuel_max_orig);
     return descriptor;
 }
@@ -105,7 +124,8 @@ descriptor::Base generateDriveDescriptor(int race, int tech_level)
     int modules_num_max        = meti::getRandInt(EQUIPMENT::DRIVE::MODULES_NUM_MIN, EQUIPMENT::DRIVE::MODULES_NUM_MAX);
     int mass                   = meti::getRandInt(EQUIPMENT::DRIVE::MASS_MIN, EQUIPMENT::DRIVE::MASS_MAX);
     int condition_max          = meti::getRandInt(EQUIPMENT::DRIVE::CONDITION_MIN, EQUIPMENT::DRIVE::CONDITION_MAX);
-    int deterioration_normal = 1;
+    int deterioration = 1;
+    int price = meti::getRandInt(100, 1000);
 
     int speed = meti::getRandInt(EQUIPMENT::DRIVE::SPEED_MIN, EQUIPMENT::DRIVE::SPEED_MAX) * (1 + EQUIPMENT::DRIVE::SPEED_TECH_RATE * (int)tech_level);
     int hyper = meti::getRandInt(EQUIPMENT::DRIVE::HYPER_MIN, EQUIPMENT::DRIVE::HYPER_MAX) * (1 + EQUIPMENT::DRIVE::HYPER_TECH_RATE * (int)tech_level);
@@ -113,14 +133,10 @@ descriptor::Base generateDriveDescriptor(int race, int tech_level)
     //jeti::TextureOb* texOb_item = TextureCollector::Instance().getTextureByTypeId(TYPE::TEXTURE::DRIVE_EQUIPMENT_ID);
 
     descriptor::Base descriptor;
-    descriptor.add(descriptor::Key::ID, global::get().idGenerator().nextId());
-    descriptor.add(descriptor::Key::TYPE, int(descriptor::Base::Type::DRIVE));
-    descriptor.add(descriptor::Key::RACE, race);
-    descriptor.add(descriptor::Key::TECH, tech_level);
-    descriptor.add(descriptor::Key::MODULES_NUM, modules_num_max);
-    descriptor.add(descriptor::Key::MASS, mass);
-    descriptor.add(descriptor::Key::CONDITION, condition_max);
-    descriptor.add(descriptor::Key::DETERIORATION, deterioration_normal);
+    addItemCommonFields(descriptor,
+                        global::get().idGenerator().nextId(),
+                        int(descriptor::Base::Type::DRIVE),
+                        race, tech_level, modules_num_max, mass, condition_max, deterioration, price);
     descriptor.add(descriptor::Key::SPEED, speed);
     descriptor.add(descriptor::Key::HYPER, hyper);
     return descriptor;
@@ -138,21 +154,18 @@ descriptor::Base generateDroidDescriptor(int race, int tech_level)
     int modules_num_max = meti::getRandInt(EQUIPMENT::DROID::MODULES_NUM_MIN, EQUIPMENT::DROID::MODULES_NUM_MAX);
     int mass            = meti::getRandInt(EQUIPMENT::DROID::MASS_MIN,        EQUIPMENT::DROID::MASS_MAX);
     int condition_max   = meti::getRandInt(EQUIPMENT::DROID::CONDITION_MIN,   EQUIPMENT::DROID::CONDITION_MAX);
-    int deterioration_normal = 1;
+    int deterioration = 1;
+    int price = meti::getRandInt(100, 1000);
 
     int repair = meti::getRandInt(EQUIPMENT::DROID::REPAIR_MIN, EQUIPMENT::DROID::REPAIR_MAX) * (1 + EQUIPMENT::DROID::REPAIR_TECH_RATE*(int)tech_level);
     //jeti::Mesh* mesh = MeshCollector::Instance().getMesh(TYPE::MESH::PLANE_ID);
     //jeti::TextureOb* texOb_item = TextureCollector::Instance().getTextureByTypeId(TYPE::TEXTURE::DROID_EQUIPMENT_ID);
 
     descriptor::Base descriptor;
-    descriptor.add(descriptor::Key::ID, global::get().idGenerator().nextId());
-    descriptor.add(descriptor::Key::TYPE, int(descriptor::Base::Type::DROID));
-    descriptor.add(descriptor::Key::RACE, race);
-    descriptor.add(descriptor::Key::TECH, tech_level);
-    descriptor.add(descriptor::Key::MODULES_NUM, modules_num_max);
-    descriptor.add(descriptor::Key::MASS, mass);
-    descriptor.add(descriptor::Key::CONDITION, condition_max);
-    descriptor.add(descriptor::Key::DETERIORATION, deterioration_normal);
+    addItemCommonFields(descriptor,
+                        global::get().idGenerator().nextId(),
+                        int(descriptor::Base::Type::DROID),
+                        race, tech_level, modules_num_max, mass, condition_max, deterioration, price);
     descriptor.add(descriptor::Key::REPAIR, repair);
     return descriptor;
 }
@@ -169,7 +182,8 @@ descriptor::Base generateGrappleDescriptor(int race, int tech_level)
     int modules_num_max = meti::getRandInt(EQUIPMENT::GRAPPLE::MODULES_NUM_MIN, EQUIPMENT::GRAPPLE::MODULES_NUM_MAX);
     int mass            = meti::getRandInt(EQUIPMENT::GRAPPLE::MASS_MIN,        EQUIPMENT::GRAPPLE::MASS_MAX);
     int condition_max   = meti::getRandInt(EQUIPMENT::GRAPPLE::CONDITION_MIN,   EQUIPMENT::GRAPPLE::CONDITION_MAX);
-    int deterioration_normal = 1;
+    int deterioration = 1;
+    int price = meti::getRandInt(100, 1000);
 
     //jeti::Mesh* mesh = MeshCollector::Instance().getMesh(TYPE::MESH::PLANE_ID);
     //jeti::TextureOb* texOb_item = TextureCollector::Instance().getTextureByTypeId(TYPE::TEXTURE::GRAPPLE_EQUIPMENT_ID);
@@ -179,14 +193,10 @@ descriptor::Base generateGrappleDescriptor(int race, int tech_level)
     int speed      = meti::getRandInt(EQUIPMENT::GRAPPLE::SPEED_MIN,    EQUIPMENT::GRAPPLE::SPEED_MAX)    * (1 + EQUIPMENT::GRAPPLE::SPEED_TECH_RATE * (int)tech_level);
 
     descriptor::Base descriptor;
-    descriptor.add(descriptor::Key::ID, global::get().idGenerator().nextId());
-    descriptor.add(descriptor::Key::TYPE, int(descriptor::Base::Type::GRAPPLE));
-    descriptor.add(descriptor::Key::RACE, race);
-    descriptor.add(descriptor::Key::TECH, tech_level);
-    descriptor.add(descriptor::Key::MODULES_NUM, modules_num_max);
-    descriptor.add(descriptor::Key::MASS, mass);
-    descriptor.add(descriptor::Key::CONDITION, condition_max);
-    descriptor.add(descriptor::Key::DETERIORATION, deterioration_normal);
+    addItemCommonFields(descriptor,
+                        global::get().idGenerator().nextId(),
+                        int(descriptor::Base::Type::GRAPPLE),
+                        race, tech_level, modules_num_max, mass, condition_max, deterioration, price);
     descriptor.add(descriptor::Key::STRENGTH, strength);
     descriptor.add(descriptor::Key::RADIUS, radius);
     descriptor.add(descriptor::Key::SPEED, speed);
@@ -206,7 +216,8 @@ descriptor::Base generateScanerDescriptor(int race, int tech_level)
     int modules_num_max = meti::getRandInt(EQUIPMENT::SCANER::MODULES_NUM_MIN, EQUIPMENT::SCANER::MODULES_NUM_MAX);
     int mass            = meti::getRandInt(EQUIPMENT::SCANER::MASS_MIN,        EQUIPMENT::SCANER::MASS_MAX);
     int condition_max   = meti::getRandInt(EQUIPMENT::SCANER::CONDITION_MIN,   EQUIPMENT::SCANER::CONDITION_MAX);
-    int deterioration_normal = 1;
+    int deterioration = 1;
+    int price = meti::getRandInt(100, 1000);
 
     //jeti::Mesh* mesh = MeshCollector::Instance().getMesh(TYPE::MESH::PLANE_ID);
     //jeti::TextureOb* texOb_item = TextureCollector::Instance().getTextureByTypeId(TYPE::TEXTURE::SCANER_EQUIPMENT_ID);
@@ -214,14 +225,10 @@ descriptor::Base generateScanerDescriptor(int race, int tech_level)
     int scan = meti::getRandInt(EQUIPMENT::SCANER::SCAN_MIN, EQUIPMENT::SCANER::SCAN_MAX) * (1 + EQUIPMENT::SCANER::SCAN_TECH_RATE * (int)tech_level);
 
     descriptor::Base descriptor;
-    descriptor.add(descriptor::Key::ID, global::get().idGenerator().nextId());
-    descriptor.add(descriptor::Key::TYPE, int(descriptor::Base::Type::SCANER));
-    descriptor.add(descriptor::Key::RACE, race);
-    descriptor.add(descriptor::Key::TECH, tech_level);
-    descriptor.add(descriptor::Key::MODULES_NUM, modules_num_max);
-    descriptor.add(descriptor::Key::MASS, mass);
-    descriptor.add(descriptor::Key::CONDITION, condition_max);
-    descriptor.add(descriptor::Key::DETERIORATION, deterioration_normal);
+    addItemCommonFields(descriptor,
+                        global::get().idGenerator().nextId(),
+                        int(descriptor::Base::Type::SCANER),
+                        race, tech_level, modules_num_max, mass, condition_max, deterioration, price);
     descriptor.add(descriptor::Key::SCAN, scan);
 
     return descriptor;
@@ -239,7 +246,8 @@ descriptor::Base generateRadarDescriptor(int race, int tech_level)
     int modules_num_max = meti::getRandInt(EQUIPMENT::RADAR::MODULES_NUM_MIN, EQUIPMENT::RADAR::MODULES_NUM_MAX);
     int mass            = meti::getRandInt(EQUIPMENT::RADAR::MASS_MIN,        EQUIPMENT::RADAR::MASS_MAX);
     int condition_max   = meti::getRandInt(EQUIPMENT::RADAR::CONDITION_MIN,   EQUIPMENT::RADAR::CONDITION_MAX);
-    int deterioration_normal = 1;
+    int deterioration = 1;
+    int price = meti::getRandInt(100, 1000);
 
     //jeti::Mesh* mesh = MeshCollector::Instance().getMesh(TYPE::MESH::PLANE_ID);
     //jeti::TextureOb* texOb_item = TextureCollector::Instance().getTextureByTypeId(TYPE::TEXTURE::RADAR_EQUIPMENT_ID);
@@ -247,14 +255,10 @@ descriptor::Base generateRadarDescriptor(int race, int tech_level)
     int radius = meti::getRandInt(EQUIPMENT::RADAR::RADIUS_MIN, EQUIPMENT::RADAR::RADIUS_MAX);
 
     descriptor::Base descriptor;
-    descriptor.add(descriptor::Key::ID, global::get().idGenerator().nextId());
-    descriptor.add(descriptor::Key::TYPE, int(descriptor::Base::Type::RADAR));
-    descriptor.add(descriptor::Key::RACE, race);
-    descriptor.add(descriptor::Key::TECH, tech_level);
-    descriptor.add(descriptor::Key::MODULES_NUM, modules_num_max);
-    descriptor.add(descriptor::Key::MASS, mass);
-    descriptor.add(descriptor::Key::CONDITION, condition_max);
-    descriptor.add(descriptor::Key::DETERIORATION, deterioration_normal);
+    addItemCommonFields(descriptor,
+                        global::get().idGenerator().nextId(),
+                        int(descriptor::Base::Type::RADAR),
+                        race, tech_level, modules_num_max, mass, condition_max, deterioration, price);
     descriptor.add(descriptor::Key::RADIUS, radius);
 
     return descriptor;
@@ -272,7 +276,8 @@ descriptor::Base generateProtectorDescriptor(int race, int tech_level)
     int modules_num_max = meti::getRandInt(EQUIPMENT::PROTECTOR::MODULES_NUM_MIN, EQUIPMENT::PROTECTOR::MODULES_NUM_MAX);
     int mass            = meti::getRandInt(EQUIPMENT::PROTECTOR::MASS_MIN,        EQUIPMENT::PROTECTOR::MASS_MAX);
     int condition_max   = meti::getRandInt(EQUIPMENT::PROTECTOR::CONDITION_MIN,   EQUIPMENT::PROTECTOR::CONDITION_MAX);
-    int deterioration_normal = 1;
+    int deterioration = 1;
+    int price = meti::getRandInt(100, 1000);
 
     //jeti::Mesh* mesh = MeshCollector::Instance().getMesh(TYPE::MESH::PLANE_ID);
     //jeti::TextureOb* texOb_item = TextureCollector::Instance().getTextureByTypeId(TYPE::TEXTURE::PROTECTOR_EQUIPMENT_ID);
@@ -280,14 +285,10 @@ descriptor::Base generateProtectorDescriptor(int race, int tech_level)
     int protection = meti::getRandInt(EQUIPMENT::PROTECTOR::PROTECTION_MIN, EQUIPMENT::PROTECTOR::PROTECTION_MAX);
 
     descriptor::Base descriptor;
-    descriptor.add(descriptor::Key::ID, global::get().idGenerator().nextId());
-    descriptor.add(descriptor::Key::TYPE, int(descriptor::Base::Type::PROTECTOR));
-    descriptor.add(descriptor::Key::RACE, race);
-    descriptor.add(descriptor::Key::TECH, tech_level);
-    descriptor.add(descriptor::Key::MODULES_NUM, modules_num_max);
-    descriptor.add(descriptor::Key::MASS, mass);
-    descriptor.add(descriptor::Key::CONDITION, condition_max);
-    descriptor.add(descriptor::Key::DETERIORATION, deterioration_normal);
+    addItemCommonFields(descriptor,
+                        global::get().idGenerator().nextId(),
+                        int(descriptor::Base::Type::PROTECTOR),
+                        race, tech_level, modules_num_max, mass, condition_max, deterioration, price);
     descriptor.add(descriptor::Key::PROTECTION, protection);
 
     return descriptor;
