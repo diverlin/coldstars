58173971b1941ee7e8e5522c40b4da47622f6ffd
Oleksandr Pivovarov <Oleksandr Pivovarov@localhost>
2016-10-30 09:16:45 +0200

    checkpoint

--- src/game/client/view/SpaceView.cpp
+++ src/game/client/view/SpaceView.cpp
@@ -16,7 +16,7 @@
     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
-#include "RenderManager.hpp"
+#include "SpaceView.hpp"
 #include <jeti/constants.hpp>
 
 #include <ceti/Collision.hpp>
