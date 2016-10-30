ffd76199c5b3f7e11fc2a00743982608d78e6b00
Oleksandr Pivovarov <aleksandr.pivovarov.84@gmail.com>
2016-10-30 03:07:33 +0300

    checkpoint

--- src/game/core/builder/spaceobjects/PlanetBuilder.cpp
+++ src/game/core/builder/spaceobjects/PlanetBuilder.cpp
@@ -50,32 +50,34 @@ Planet::__createTemplate(int_t id)
 model::Planet*
 Planet::getNew()
 {
-    const auto& descr = global::get().descriptors().getRand(descriptor::Type::PLANET);
+    const auto& descr = global::get().descriptors().planet().random();
     return getNew(descr);
 } 
      
 model::Planet*
-Planet::getNew(const descriptor::BaseOLD& descr)
+Planet::getNew(const descriptor::Planet& descr)
 {
     model::Planet* model = __createTemplate();
 
     //jeti::Mesh* mesh = MeshCollector::Instance().getMesh(TYPE::MESH::SPHERE_ID);
     //jeti::TextureOb* textureOb      = TextureCollector::Instance().getTextureByTypeId(TYPE::TEXTURE::PLANET_ID);
 
-    int orbit_radius = 1000; // hack
-    //model->setOrbitCenter(glm::vec3(0, 0, DEFAULT_ENTITY_ZPOS));
-    model->setRadiusA(orbit_radius);
-    model->setRadiusB(orbit_radius);
-    model->setOrbitPhi(0);
-    model->setSpeed(meti::getRandInt(model::Planet::SPEED_MIN, model::Planet::SPEED_MAX) / float(orbit_radius));
-    model->setClockwise(meti::getRandBool());
+    /// Planetoid
+    model->setRadiusA(descr.radiusA());
+    model->setRadiusB(descr.radiusB());
+    model->setOrbitPhi(descr.orbitPhi());
+    model->setSpeed(descr.speed());
+    model->setClockwise(descr.clockwise());
 
+    /// Orientation
+
+    ///
     LifeData data_life;
     data_life.armor = descr.armor();
     model->setLifeData(data_life);
 
-    float scale_comp = meti::getRandInt(model::Planet::SCALE_MIN, model::Planet::SCALE_MAX);
-    glm::vec3 scale(scale_comp, scale_comp, scale_comp);
+    //float scale_comp = meti::getRandInt(model::Planet::SCALE_MIN, model::Planet::SCALE_MAX);
+    //glm::vec3 scale(scale_comp, scale_comp, scale_comp);
     //alpitodorender planet->SetRenderData(mesh, textureOb, scale);
 
     float delta_angle = 0.0001*meti::getRandInt(20, 60);
@@ -104,5 +106,60 @@ Planet::getNew(const descriptor::BaseOLD& descr)
     */
     return model;
 }
+
+
+
+
+//model::Planet*
+//Planet::getNew(const descriptor::BaseOLD& descr)
+//{
+//    model::Planet* model = __createTemplate();
+
+//    //jeti::Mesh* mesh = MeshCollector::Instance().getMesh(TYPE::MESH::SPHERE_ID);
+//    //jeti::TextureOb* textureOb      = TextureCollector::Instance().getTextureByTypeId(TYPE::TEXTURE::PLANET_ID);
+
+//    int orbit_radius = 1000; // hack
+//    //model->setOrbitCenter(glm::vec3(0, 0, DEFAULT_ENTITY_ZPOS));
+//    model->setRadiusA(orbit_radius);
+//    model->setRadiusB(orbit_radius);
+//    model->setOrbitPhi(0);
+//    model->setSpeed(meti::getRandInt(model::Planet::SPEED_MIN, model::Planet::SPEED_MAX) / float(orbit_radius));
+//    model->setClockwise(meti::getRandBool());
+
+//    LifeData data_life;
+//    data_life.armor = descr.armor();
+//    model->setLifeData(data_life);
+
+//    float scale_comp = meti::getRandInt(model::Planet::SCALE_MIN, model::Planet::SCALE_MAX);
+//    glm::vec3 scale(scale_comp, scale_comp, scale_comp);
+//    //alpitodorender planet->SetRenderData(mesh, textureOb, scale);
+
+//    float delta_angle = 0.0001*meti::getRandInt(20, 60);
+//    //jeti::AnimationConstantRotation* animation_rotation = new jeti::AnimationConstantRotation(delta_angle);
+//    //alpitodorender planet->SetAnimationRotation(animation_rotation);
+
+//    //if (getRandBool())
+//    {
+//        //jeti::TextureOb* textureOb = TextureCollector::Instance().getTextureByTypeId(TYPE::TEXTURE::ATMOSPHERE_ID);
+//        //Atmosphere* atmosphere = new Atmosphere();
+//        //alpitodorender atmosphere->SetRenderData(mesh, textureOb, 1.04f*scale);
+//        //alpitodorender planet->AddDecoration(atmosphere);
+//    }
+
+//   /*
+//   {
+//        Mesh* mesh_plane = MeshCollector::Instance().getMesh(TYPE::MESH::PLANE_ID);
+
+//        TextureOb* textureOb = TextureCollector::Instance().getTextureByTypeId(TYPE::TEXTURE::RING_ID);
+//        Ring* ring = new Ring();
+//        ring->BindData3D(mesh_plane, textureOb, 1.5f*scale);
+//        //ring->setDirection(glm::normalize(glm::vec3(1.0f)));
+//        ring->setDirection(glm::normalize(glm::vec3(0.0f, 1.0f, 0.0f)));
+//        planet->AddDecoration(ring);
+//    }
+//    */
+//    return model;
+//}
+
 
 } // namespace builder
