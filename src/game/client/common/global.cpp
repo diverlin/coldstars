5d77033e34b2c26c6207c2555f3638e29c9ae015
Oleksandr Pivovarov <aleksandr.pivovarov.84@gmail.com>
2016-12-31 00:06:42 +0200

    checkpoint

---
+++ src/game/client/common/global.cpp
@@ -0,0 +1,54 @@
+/*
+    Copyright (C) ColdStars, Aleksandr Pivovarov <<coldstars8@gmail.com>>
+    
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License
+    as published by the Free Software Foundation; either version 2
+    of the License, or (at your option) any later version.
+    
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+#include "global.hpp"
+#include <jeti/Screen.hpp>
+#include <jeti/Render.hpp>
+#include <jeti/Camera.hpp>
+
+namespace client
+{
+
+global& global::get()
+{
+    static global instance;
+    return instance;
+}
+
+global::global()
+    :
+      m_screen(new jeti::Screen)
+    , m_render(new jeti::Renderer)
+    , m_camera(new jeti::Camera)
+{
+}
+
+global::~global()
+{
+    delete m_screen;
+    delete m_render;
+    delete m_camera;
+}
+
+void
+global::init() {
+    m_screen->init();
+    m_render->init(m_camera, m_screen->width(), m_screen->height());
+}
+
+} // namespace client
