0c36ed84cb3a82e91185abe01f6b2dccc18d8c4f
Oleksandr Pivovarov <aleksandr.pivovarov.84@gmail.com>
2017-01-22 18:39:42 +0200

    checkpoint

--- src/game/core/test/helper.cpp
+++ src/game/core/test/helper.cpp
@@ -16,7 +16,7 @@
      Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
-#include <common/Global.hpp>
+#include <core/common/Global.hpp>
 #include <ceti/IdGenerator.hpp>
 #include <world/starsystem.hpp>
 #include <item/others/Bomb.hpp>
