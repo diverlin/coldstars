d663a2c568a6faea17409c9fbda282d6fa717fe8
Oleksandr Pivovarov <Oleksandr Pivovarov@localhost>
2017-04-01 01:27:33 +0300

    checkpoint

---
+++ src/game/core/src/core/model/dock/Room.hpp
@@ -0,0 +1,47 @@
+/*
+    Copyright (C) ColdStars, Aleksandr Pivovarov <<coldstars8@gmail.com>>
+    
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License
+    as published by the Free Software Foundation; either version 2
+    of the License, or (at your option) any later version.
+    
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+
+#pragma once
+
+#include <core/common/Base.hpp>
+
+namespace model {
+
+class Room : public Base {
+public:
+    Room() = default;
+    ~Room() = default;
+
+    void setKosmoport(int_t kosmoport) { m_kosmoport = kosmoport; }
+
+    int_t kosmoport() const { return m_kosmoport; }
+
+private:
+    friend class boost::serialization::access;
+    template<class Archive>
+    void serialize(Archive & ar, const unsigned int version) {
+        ar & boost::serialization::base_object<Base>(*this);
+        ar & m_kosmoport;
+    }
+
+private:
+    int_t m_kosmoport = NONE;
+};
+
+} // namespace model
