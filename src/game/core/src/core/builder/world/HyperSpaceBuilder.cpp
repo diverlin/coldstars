f085d297ad1018881342b34a475725734140b064
Oleksandr Pivovarov <Oleksandr Pivovarov@localhost>
2017-04-04 13:14:05 +0300

    add missing files

---
+++ src/game/core/src/core/builder/world/HyperSpaceBuilder.cpp
@@ -0,0 +1,75 @@
+/*
+    Copyright (C) ColdStars, Aleksandr Pivovarov <<coldstars8@gmail.com>>
+    
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License
+    as published by the Free Software Foundation; either version 2
+    of the License, or (at your option) any later version.
+    
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+
+#include "HyperSpaceBuilder.hpp"
+
+#include <core/type/DescriptorType.hpp>
+
+#include <core/descriptor/world/HyperSpace.hpp>
+#include <core/model/world/HyperSpace.hpp>
+#include <core/world/HyperSpace.hpp>
+
+#include <core/manager/DescriptorManager.hpp>
+#include <core/generator/DescriptorGenerator.hpp>
+#include <core/manager/EntityManager.hpp>
+
+namespace builder {
+
+control::HyperSpace*
+HyperSpace::gen()
+{
+    descriptor::HyperSpace* descr = nullptr;
+    if (!descriptor::Manager::get().hasType(descriptor::Type::HYPERSPACE)) {
+        descr = descriptor::genHyperSpace();
+    } else {
+        descr = descriptor::Manager::get().randHyperSpace();
+    }
+    assert(descr);
+    return gen(descr);
+}
+
+control::HyperSpace*
+HyperSpace::gen(descriptor::HyperSpace* descr)
+{
+    control::HyperSpace* hyperspace = __genTemplate(descr);
+    return hyperspace;
+} 
+
+control::HyperSpace*
+HyperSpace::gen(int_t descriptor_id, int_t ob_id)
+{
+    descriptor::HyperSpace* descr = descriptor::Manager::get().hyperSpace(descriptor_id);
+    control::HyperSpace* hyperspace = __genTemplate(descr, ob_id);
+    return hyperspace;
+}
+
+control::HyperSpace*
+HyperSpace::__genTemplate(descriptor::HyperSpace* descr, int_t ob_id)
+{
+    model::HyperSpace* model = new model::HyperSpace(descr->id(), ob_id);
+    assert(model);
+
+    control::HyperSpace* hyperspace = new control::HyperSpace(descr, model);
+    assert(hyperspace);
+
+    EntityManager::get().setHyperSpace(hyperspace);
+    return hyperspace;
+}
+
+} // namespace builder
---
+++ src/game/core/src/core/model/world/HyperSpace.hpp
@@ -0,0 +1,54 @@
+/*
+     Copyright (C) ColdStars, Aleksandr Pivovarov <<coldstars8@gmail.com>>
+
+     This program is free software; you can redistribute it and/or
+     modify it under the terms of the GNU General Public License
+     as published by the Free Software Foundation; either version 2
+     of the License, or (at your option) any later version.
+
+     This program is distributed in the hope that it will be useful,
+     but WITHOUT ANY WARRANTY; without even the implied warranty of
+     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+     GNU General Public License for more details.
+
+     You should have received a copy of the GNU General Public License
+     along with this program; if not, write to the Free Software
+     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+#pragma once
+
+#include <core/common/Base.hpp>
+
+#include <ceti/Pack.hpp>
+
+namespace model {
+
+class HyperSpace : public Base
+{
+public:
+    HyperSpace(int_t, int_t ob_id = NONE);
+    ~HyperSpace() = default;
+    HyperSpace(const std::string& data);
+    std::string data() const;
+
+    void addVehicle(int_t ship) { if (_isWritable()) m_vehicles.add(ship); }
+
+    void removeVehicle(int_t id) { m_vehicles.remove(id); }
+
+    ceti::pack<int_t> ships() const { return m_vehicles; }
+
+private:
+    ceti::pack<int_t> m_vehicles;
+
+private:
+    friend class boost::serialization::access;
+    template<class Archive>
+    void serialize(Archive & ar, const unsigned int version) {
+        ar & boost::serialization::base_object<Base>(*this);
+        ar & m_vehicles;
+    }
+};
+
+} // namespace model
+
