30aab42bc656fb1e87379f00814adf053e2fdf1b
Oleksandr Pyvovarov <aleksandr.pivovarov.84@gmail.com>
2017-11-10 14:05:56 +0200

    checkpoint

---
+++ src/jeti/app/controlwidget.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <QWidget>
+
+class QLineEdit;
+class QSlider;
+class QSpinBox;
+
+class ControlWidget: public QWidget
+{
+    Q_OBJECT
+public:
+    ControlWidget(int min, int max, QWidget* parent = nullptr);
+    ~ControlWidget()=default;
+
+    int value() const { return m_value; }
+
+    void setValue(int value);
+
+private slots:
+    void __slot_setValue(int value);
+
+signals:
+    void valueChanged(int value);
+
+private:
+    int m_value = 0;
+    QLineEdit* m_lineEdit = nullptr;
+    QSlider* m_slider = nullptr;
+    QSpinBox* m_spinBox = nullptr;
+};
+
+
+
