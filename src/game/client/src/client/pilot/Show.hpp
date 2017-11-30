9e87905bc1e4c0711265d7e2a8e4202f37b5c696
Oleksandr Pyvovarov <Oleksandr Pyvovarov@localhost>
2017-11-30 14:27:25 +0200

    checkpoint

--- src/game/client/src/client/pilot/Show.hpp
+++ src/game/client/src/client/pilot/Show.hpp
@@ -45,7 +45,7 @@ public:
 private:
     bool m_star = true;
     bool m_stars = true;
-    bool m_nebulas = false;
+    bool m_nebulas = true;
     bool m_spaceobjects = true;
     bool m_collision_radius = true;
     bool m_axis = false;
