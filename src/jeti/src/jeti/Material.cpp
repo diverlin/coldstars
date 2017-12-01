7ee18f1db9ff8b49a40ae3c863fa0dd7a52e9c34
Oleksandr Pyvovarov <aleksandr.pivovarov.84@gmail.com>
2017-12-01 14:17:54 +0200

    fix texture filtering

--- src/jeti/src/jeti/Material.cpp
+++ src/jeti/src/jeti/Material.cpp
@@ -41,8 +41,8 @@ void loadToVRAM(GLuint& texture, const sf::Uint8* data, int& w, int& h)
 
     gluBuild2DMipmaps(GL_TEXTURE_2D, GL_RGBA, w, h, GL_RGBA, GL_UNSIGNED_BYTE, data);
 
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
     CHECK_OPENGL_ERRORS
 }
