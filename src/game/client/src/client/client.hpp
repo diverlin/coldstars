e72fb928d0d8fdfdd8f7adb78f042a35ef025ddf
Oleksandr Pyvovarov <Oleksandr Pyvovarov@localhost>
2018-03-18 20:28:37 +0200

    cleanup

--- src/game/client/src/client/client.hpp
+++ src/game/client/src/client/client.hpp
@@ -16,69 +16,28 @@
      Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
-#include <iostream>
-
-#include "builder/world/GalaxyBuilder.hpp"
-#include <common/Config.hpp>
-
-#include <core/communication/TelegramCreator.hpp>
-#include <core/communication/TelegramHandler.hpp>
-#include <core/session/Session.hpp>
-#include <core/session/Sessions.hpp>
-#include <core/session/Shortcuts.hpp>
-
-#include <jeti/Mesh.hpp>
-#include <jeti/Screen.hpp>
-#include <jeti/Camera.hpp>
-#include <jeti/GlErrorHelper.hpp>
-
-#include <meti/RandUtils.hpp>
-
-#include <client/session/Session.hpp>
-#include <client/session/Shortcuts.hpp>
-#include <client/resources/Data.hpp>
-#include <client/gui/UserInputManagerInSpace.hpp>
-#include <client/gui/UserInput.hpp>
-
-#include <client/gui/ButtonTrigger.hpp>
-#include <client/gui/GuiActions.hpp>
-
-#include "common/TurnTimer.hpp"
-
-#include <core/pilot/Npc.hpp>
-#include <client/pilot/Player.hpp>
-
-#include <core/spaceobject/Planet.hpp>
-#include <core/spaceobject/Vehicle.hpp>
-#include <core/spaceobject/Ship.hpp>
-
-#include <common/Global.hpp>
-#include <core/manager/EntityManager.hpp>
-
-#include <world/galaxy.hpp>
-#include <world/Sector.hpp>
-#include <world/starsystem.hpp>
-#include <Game.hpp>
-#include <ai/God.hpp>
-
-#include <core/descriptor/world/GalaxyDescriptor.hpp>
-#include <core/manager/DescriptorManager.hpp>
-#include "dock/Land.hpp"
-
-#include "run_scenario/NormalRunScenario.hpp"
-#include "run_scenario/TestParticlesRunScenario.hpp"
-#include "run_scenario/TestTextRunScenario.hpp"
-#include "run_scenario/TestDrawManySimpleMeshesRunScenario.hpp"
-
-#include <client/view/StarSystem.hpp>
-
-#include <core/communication/TelegramHub.hpp>
-#include <client/communication/TelegramHandler.hpp>
-
-#include <client/gui/GuiManager.hpp>
-
 #include <memory>
 
+namespace client {
+class Player;
+namespace comm {
+class TelegramHandler;
+} // namespace comm
+} // namespace client
+
+namespace jeti {
+class Camera;
+class Render;
+class Screen;
+} // namespace jeti
+
+namespace view {
+class StarSystem;
+} // namespace view
+
+namespace gui {
+class UserInputInSpace;
+} // namespace gui
 
 class Client {
 public:
